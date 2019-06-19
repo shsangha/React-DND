fb00289736196b8706c5ed7381a90c8fdea21e5c
shsangha <sssangha@ucalgary.ca>
2019-06-19 15:07:26 -0600

    updated sourcemap option to be able to properly trace callstack to make sure race conditions in order of event calls were avoided

---
+++ webpack-dev.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.[hash].js",
+    publicPath: "/"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "awesome-typescript-loader"
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: "graphql-tag/loader"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /\.module\.scss$/,
+        loader: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.module\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true
+            }
+          },
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+      filename: "index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/])
+  ],
+  devServer: {
+    host: "0.0.0.0",
+    port: 3000,
+    hot: true
+  },
+  watch: true,
+  watchOptions: {
+    poll: 1000
+  }
+};
