5a438d43e2c1770f76c47a8cd4911d27feb6806f
shsangha <sssangha@ucalgary.ca>
2019-06-21 15:33:47 -0600

    decided to change approaches again now taking advantage of react batching of setstate in event handlers instead of using rxjs bc it doesnt batch

--- src/context.tsx
+++ src/context.tsx
@@ -1,58 +1,84 @@
 import React, { Component, createContext, createRef } from "react";
 
 interface Props {
-  children: () => React.ReactNode;
-}
-
-interface Context {
-  data: any[];
-  updateContext: (update: object, callback: () => void) => void;
+  scroll: boolean;
+  srollSensitivity: number;
+  children: (a: any) => React.ReactNode;
 }
 
 interface State {
-  origin: number | null;
-  current: number | null;
-  previous: number | null;
-  type: string;
+  data: any[];
+  origin: { id: number; index: number } | null;
+  over: number | null;
+  currentPosition: number | null;
 }
+
+interface Context {
+  updateDragData: (state: Partial<State>, props: Partial<Props>) => void;
+  updateDragContext: (state: State) => void;
+  move: () => void;
+}
+
+const defaultState = {
+  data: [1, 2, 3, 4, 5],
+  origin: null,
+  over: null,
+  currentPosition: null
+};
 
 export const DragContext: React.Context<any> = createContext({});
 
-const defaultState = {
-  origin: null,
-  current: null,
-  previous: null,
-  type: ""
-};
-
-// will need to hold a ref to the draggable context for scrolling
-
-export default class DragEnabledContainer extends Component<Props> {
-  public DragContextRef: React.RefObject<any> = createRef();
+export default class DragEnabledContainer extends Component<Props, State> {
+  public containerRef: React.RefObject<any> = createRef();
 
   public state = { ...defaultState };
 
-  public updateDragContext = (changes: object, callback: any) => {
-    this.setState(
-      prevState => ({
-        ...prevState,
-        ...changes
-      }),
-      () => {
-        callback(this.state);
-      }
-    );
+  public scroll = () => {
+    return true;
+  };
+
+  public checkInContainer = () => {
+    return () => {
+      console.log("rinng");
+    };
+  };
+
+  public moveTo = (origin: number, target: number) => {
+    this.setState(prevState => {
+      const data = [...prevState.data];
+      data.splice(origin, 1);
+      data.splice(target, 0, prevState.data[origin]);
+      return {
+        data,
+        currentPosition: target
+      };
+    });
+  };
+
+  public swap = (origin: number, target: number) => {
+    this.setState(prevState => {
+      const data = [...prevState.data];
+
+      data[origin] = prevState.data[target];
+      data[target] = prevState.data[origin];
+
+      return {
+        data
+      };
+    });
   };
 
   public render() {
     return (
-      <div ref={this.DragContextRef}>
+      <div ref={this.containerRef}>
         <DragContext.Provider
           value={{
-            updateDragContext: this.updateDragContext
+            scroll: this.scroll,
+            checkInContainer: this.checkInContainer,
+            move: this.componentWillReceiveProps
           }}
         >
-          {this.props.children()}
+          {this.props.children({ data: this.state.data, move: this.moveTo })}
         </DragContext.Provider>
       </div>
     );
