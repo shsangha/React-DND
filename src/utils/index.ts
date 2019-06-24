4bbb8dea41a56f380c25136724f8d55bc856a517
shsangha <sssangha@ucalgary.ca>
2019-06-24 14:50:29 -0600

    cleaned up exports from utils folder

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+import scroll from "./scroll";
+import createDragEvent from "./createDragEvent";
+import { getDroppable } from "./getTarget";
+import noop from "./noop";
+import partial from "./partial";
+import resize from "./resize";
+
+export { scroll, createDragEvent, getDroppable, noop, partial, resize };
