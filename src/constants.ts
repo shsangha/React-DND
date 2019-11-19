d31456249b80f28bc1f9a60d6efeabcdc5a60ac4
shsangha <sssangha@ucalgary.ca>
2019-11-18 20:02:38 -0700

    pulled constants for ids out into one file to remain consitent

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const DRAGGING_ELEMENT_ID = "react_dnd_dragging_element_id";
 
 export const PLACEHOLDER_ID = "react_dnd_placeholder_element_id";
+
+export const ASSITIVE_TEXT_ID = "react-dnd-assistive-text";
