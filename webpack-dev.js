662e690667895cfb69787d6e32711169d1d97ad2
shsangha <sssangha@ucalgary.ca>
2019-11-19 11:57:58 -0700

    added historyfallback api to dev-server config to fix routing breaking on hot reloads

--- webpack-dev.js
+++ webpack-dev.js
@@ -57,7 +57,9 @@ module.exports = {
   devServer: {
     host: "0.0.0.0",
     port: 3000,
-    hot: true
+    hot: true,
+    historyApiFallback: true,
+    publicPath: "/"
   },
   watch: true,
   watchOptions: {
