11f7b0bcc07e5a1691e2e53ccfb4347eafc0d2ff
shsangha <sssangha@ucalgary.ca>
2019-11-18 20:10:42 -0700

    util to trigger customevent that is picked up by the droppable event handlers

--- src/utils/createEvent.ts
+++ src/utils/createEvent.ts
@@ -1,4 +1,6 @@
-export default (type: string, detail: object = {}) =>
+import { MoveDetail, KeyboardDetail } from "../types";
+
+export default (type: string, detail: MoveDetail | KeyboardDetail) =>
   new CustomEvent(type, {
     bubbles: true,
     detail
