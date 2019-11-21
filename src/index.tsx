5032bca4ecb11886052c35fc5a9402f5c68a06c6
shsangha <sssangha@ucalgary.ca>
2019-11-21 15:10:04 -0700

    base files for the cypress tests to be run against

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
 import Container from "./container";
 import Draggable from "./draggable";
 import Droppable from "./droppable";
+import { Router, RouteComponentProps, Link } from "@reach/router";
+import PlaceholderMovement from "./testComponents/PlaceholderMovement";
+import SimpleMultiContainer from "./testComponents/simpleMultiContainer";
+import Scrolling from "./testComponents/scrolling";
+import DynamicDroppable from "./testComponents/dynamicDroppables";
+
+const BehaviorTests = (path: RouteComponentProps) => {
+  const Sort = SimpleMultiContainer("sort");
+  const Swap = SimpleMultiContainer("swap");
+  const Append = SimpleMultiContainer("append");
+  const Shift = SimpleMultiContainer("shift");
+
+  return (
+    <Router primary={false}>
+      <Sort path="sort" />
+      <Swap path="swap" />
+      <Append path="append" />
+      <Shift path="shift" />
+    </Router>
+  );
+};
 
 const App = () => (
-  <div>
-    <Container>
-      {state => {
-        return (
-          <>
-            <Droppable name="name">
-              {state.values.name.map((name: any, index: any) => (
-                <Draggable key={name} index={index}>
-                  <div style={{ width: "300px" }}>{name}</div>
-                </Draggable>
-              ))}
-            </Droppable>
-            <Droppable name="age">
-              {state.values.age.map((name: any, index: any) => (
-                <Draggable key={name} index={index}>
-                  <div style={{ width: "100px" }}>{name}</div>
-                </Draggable>
-              ))}
-            </Droppable>
-            <Droppable name="smell">
-              <div
-                style={{ background: "red", width: "200px", height: "200px" }}
-              >
-                {state.values.smell.map(() => (
-                  <div>BOIIII</div>
-                ))}
-              </div>
-            </Droppable>
-            <div style={{ height: "600vh", width: "600vw" }} />
-          </>
-        );
-      }}
-    </Container>
-  </div>
+  <Router>
+    <PlaceholderMovement path="placeholderMovement" />
+    <BehaviorTests path="behavior/*" />
+    <Scrolling path="scrolling" />
+    <DynamicDroppable path="dynamicDroppable" />
+  </Router>
 );
 
 render(<App />, document.getElementById("react-root"));
