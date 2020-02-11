3799ba7ee58cbf58a857cff222cd05f79d398204
shsangha <sssangha@ucalgary.ca>
2020-02-11 11:58:28 -0700

    revised formula to factor in scroll positions

--- src/utils/createPlaceHolder.ts
+++ src/utils/createPlaceHolder.ts
@@ -1,14 +1,29 @@
 import { PLACEHOLDER_ID } from "../constants";
 
-export default (element: HTMLElement, className: string) => {
+export default (
+  element: HTMLElement,
+  className: string,
+  offsets: {
+    pageX: number;
+    pageY: number;
+    windowX: number;
+    windowY: number;
+    offsetX: number;
+    offsetY: number;
+  }
+) => {
   const placeHolder = element.cloneNode(true) as HTMLElement;
 
   const rect = element.getBoundingClientRect();
 
   requestAnimationFrame(() => {
     placeHolder.style.position = "absolute";
-    placeHolder.style.top = `${rect.top}px`;
-    placeHolder.style.left = `${rect.left}px`;
+    placeHolder.style.top = `${offsets.pageY -
+      offsets.offsetY +
+      offsets.windowY}px`;
+    placeHolder.style.left = `${offsets.pageX -
+      offsets.offsetX +
+      offsets.windowX}px`;
     placeHolder.style.pointerEvents = "none";
     placeHolder.style.touchAction = "none";
     placeHolder.style.color = "orange";
